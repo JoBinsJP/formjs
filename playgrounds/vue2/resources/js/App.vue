8f41235712cd781b909c46024c0e31131e2fc94b
Bedram Tamang <tmgbedu@gmail.com>
2023-02-06 02:30:08 +0545

    Playground

---
+++ playgrounds/vue2/resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        Hello
+        <CreateUser/>
+    </div>
+</template>
+
+<script setup>
+import CreateUser from "./CreateUser.vue";</script>
---
+++ playgrounds/vue2/resources/js/CreateUser.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="submit">
+        <label for="first_name">First name:</label>
+        <input id="first_name" v-model="form.first_name"/>
+        <label for="last_name">Last name:</label>
+        <input id="last_name" v-model="form.last_name"/>
+        <label for="email">Email:</label>
+        <input id="email" v-model="form.email"/>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+import {useForm} from "formjs-vue2"
+
+export default {
+    data() {
+        return {
+            form: useForm({
+                email: null,
+                password: null,
+                remember: false,
+            }),
+        }
+    },
+}
+</script>
