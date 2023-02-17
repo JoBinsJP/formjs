b386e36b403632b997ad6bb425e8eaccbdcda96b
Bedram Tamang <bedram@jobins.jp>
2023-02-18 01:19:29 +0545

    wip

--- packages/vue2/tests/unit/form.spec.ts
+++ packages/vue2/tests/unit/form.spec.ts
@@ -1,5 +1,5 @@
-import Axios from "axios"
 import { afterAll, afterEach, beforeAll, expect, it } from "vitest"
+import { object, string } from "yup"
 import { useForm } from "~formjs-vue2"
 import { server } from "../mocks/server"
 
@@ -25,4 +25,47 @@ it("transform method correctly transform the form data", () => {
         email: "edit@test.com",
         remember: false,
     })).data()
+})
+
+it("error method calls on validation Error", () => new Promise<void>(done => {
+    const form = useForm({
+        email: null,
+        name: null,
+    })
+
+    form.post("/api/errors", {
+        onError: () => {
+            expect(form.errors.email).toBe("The email is a required item.")
+            expect(form.errors.name).toBe("The name is a required Item.")
+
+            done()
+        },
+    })
+}))
+
+it("finish method calls on error", () => new Promise<void>(done => {
+    const form = useForm({
+        email: null,
+        name: null,
+    })
+
+    form.post("/api/errors", {
+        onFinish: () => {
+            done()
+        },
+    })
+}))
+
+it("validates form with schema", async () => {
+    const userSchema = object({
+        email: string().email(),
+    })
+
+    const form = useForm({
+        email: "admi",
+    }, userSchema)
+
+    await form.validate("email")
+
+    expect(form.errors.email).toBe("email must be a valid email")
 })
